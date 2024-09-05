c47a071b7779cb2020ab1f355d2d43b3aeed944c
22githrsh <senh2675@gmail.com>
2024-09-05 15:38:27 +0530

    frse change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+// src/App.jsx
+import React, { useEffect } from 'react';
 
 function App() {
-  window.ketchup = () => {
-    const img = document.createElement('img');
-    img.src = 'https://images.unsplash.com/photo-1598679253544-2c97992403ea?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-    document.body.appendChild(img);
-  };
+  useEffect(() => {
+    window.ketchup = () => {
+      const img = document.createElement('img');
+      img.src = 'https://images.unsplash.com/photo-1598679253544-2c97992403ea?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+      document.body.appendChild(img);
+    };
+  }, []);
 
-  return <button onClick={window.ketchup}>Show Image</button>;
+  return <button onClick={() => window.ketchup()}>Show Image</button>;
 }
 
-export default App; 
+export default App;
