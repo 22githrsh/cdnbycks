e3ad8396cddb06d32321eac1216ba030dc3a5d9c
22githrsh <senh2675@gmail.com>
2024-09-05 15:18:18 +0530

    change kardo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
-  const [imageSrc, setImageSrc] = useState('');
-
-  const ketchup = () => {
-    setImageSrc('https://images.unsplash.com/photo-1598679253544-2c97992403ea?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  window.ketchup = () => {
+    const img = document.createElement('img');
+    img.src = 'https://images.unsplash.com/photo-1598679253544-2c97992403ea?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    document.body.appendChild(img);
   };
 
-  return (
-    <div>
-      <button onClick={ketchup}>Show Image</button>
-      {imageSrc && <img src={imageSrc} alt="Ketchup Image" />}
-    </div>
-  );
+  return <button onClick={window.ketchup}>Show Image</button>;
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
