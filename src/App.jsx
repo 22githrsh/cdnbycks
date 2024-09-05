ad15d369bdbd3a6b66b6ce6c48a6717145ba761f
22githrsh <senh2675@gmail.com>
2024-09-05 16:03:51 +0530

    again

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
+    // Expose the ketchup function to the global window object
     window.ketchup = () => {
       const img = document.createElement('img');
       img.src = 'https://images.unsplash.com/photo-1598679253544-2c97992403ea?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
